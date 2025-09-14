f025c2e58e59a0f2d18322532a27a877c8f89a6a
pratomosidiq123 <pratomosidiq123@gmail.com>
2025-09-14 15:29:58 +0700

    change port to 8080

--- server.mjs
+++ server.mjs
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
